0b333474f95e68ff209c6f1be298de326c69e289
KylePixley <kyle_pixley@hotmail.com>
2023-07-18 11:08:30 -0400

    event/post models

---
+++ server-backend/models/Post.js
@@ -0,0 +1,25 @@
+const { mongoose } = require("../db");
+const { ObjectId } = mongoose.Schema;
+
+const Post = new mongoose.Schema (
+    {
+        user:{
+            type: String,
+            required: true
+        },
+        title:{
+            type: String,
+            required: true
+        },
+        body:{
+            type: String,
+            required: true
+        },
+        likes:{
+            type: Number
+        }
+    },
+    {timestamps: true }
+)
+
+module.exports = mongoose.model("post", Post);
