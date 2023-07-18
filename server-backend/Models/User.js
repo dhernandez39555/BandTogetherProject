cdd5d18f674d21290009a4a30954e2324d543116
Andy <andy.sen@icloud.com>
2023-07-18 11:03:58 -0400

    User model (#5)
    * user model completed
    
    * added timestamp

---
+++ server-backend/Models/User.js
@@ -0,0 +1,63 @@
+const { mongoose } = require("../DBConn");
+const { ObjectId } = mongoose.Schema;
+
+const User = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        contactName: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        genre: {
+            type: String
+        },
+        additionGenre: {
+            type: String
+        },
+        bio: {
+            type: String,
+            required: true
+        },
+        following: {
+            type: [ ObjectId ],
+            ref: 'user'
+        },
+        follower: {
+            type: [ ObjectId ],
+            ref: 'user'
+        },
+        friendList: {
+            type: [ ObjectId ],
+            ref: 'user'
+        },
+        socials: {
+            youtube: {
+                type: String
+            },
+            spotify: {
+                type: String
+            },
+            soundCloud: {
+                type: String
+            },
+            instagram: {
+                type: String
+            }
+        }
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("user", User);
