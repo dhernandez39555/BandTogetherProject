bea6284762ffb7493e03c96a5ded271ede36fd19
dhernandez39555 <134081390+dhernandez39555@users.noreply.github.com>
2023-07-18 11:36:17 -0400

    message.js routing (#8)
    * App.js Server spun up + nodemon command
    
    * Message routes (all in commented form)

---
+++ server-backend/Controllers/Message.js
@@ -0,0 +1,83 @@
+const router=require("express").Router()
+//Import message and user models and track user
+
+router.get("/readAllFrom/:user_id", async (req,res)=>{
+    try{
+        /*
+        const { user_id }=req.params
+        const findAll await Message.find({ sender:user_id, receiver:currentUser })
+        findAll===0?Error("You do not have any messages from that user"):null
+        res.status(200).json(findAll)
+        */
+    } catch (err){
+        // console.log(err)
+        // res.status(500).json({
+        //     message:err.message
+        // })
+    }
+})
+
+router.post("/makePostTo/:user_id", async (req,res)=>{
+    try{
+        /*
+        const { user_id }=req.params
+        const findOne=await User.findOne({user_id})
+        !findOne?Error("No users by that name exist"):null
+        const { body }=req.body
+        !body?Error("No Message Content Found")
+        const timestamp= new Date()
+        const sender=currentUser
+        const receiver= user_id
+
+        const new Message=new Message({sender,receiver,timestamp,body})
+        await newMessage.save()
+
+        res.status(200).json({
+            message:"Message sent"
+        })
+        */
+    } catch (err){
+        // console.log(err)
+        // res.status(500).json({
+        //     message:err.message
+        // })
+    }
+})
+
+router.put("/updateAt/:_id", async (req,res)=>{
+    try{
+        /*
+        const { _id }=req.params
+        const findOne=await Message.findOne({_id})
+        !findOne?Error("Message not found"):null
+        const newMessage=req.body
+        !body?Error("Message content missing"):null
+        const updateOne=await Message.updateOne({_id}, {$set:newMessage})
+        res.status.(200).json({
+            message:"Message Updated"
+        })
+        */
+    }catch (err){
+        // res.status(500).json({
+        //     message:err.message
+        // })
+    }
+})
+
+router.delete("/deleteAt/:_id", async (req,res)=>{
+    try{
+        /*
+        const{_id}=req.params
+        const deleteOne=await Message.findByIdAndDelete({_id})
+        !deleteOne?Error("ID not found")
+        res.status(200).json({
+            message:`Item Deleted`,
+            deleteOne
+        })
+        */
+    }catch (err){
+        // res.status(500).json({
+        //     message:err.message
+        // })
+    }
+})
