8fcba50ecfb0587f873f474d5ed9cf3bfe26cc4a
Dante Hernandez <hernandezdante2001@gmail.com>
2023-07-19 16:17:51 -0400

    cleaned-up client-side

--- server-backend/Controllers/Message.js
+++ server-backend/Controllers/Message.js
@@ -4,12 +4,22 @@ const User=require("../Models/User")
 
 //Import message and user models and track user
 //make currentUser have a value through token auth
-//
+//get latest post in each convo
 
-router.get("/readAllFrom/:user_id", async (req,res)=>{
+router.get("/readLatestFrom/:contactName", async (req,res)=>{
     try{
-        const { user_id }=req.params
-        const findAll=await Message.find({ sender:user_id, receiver:currentUser })
+        const { contactName }=req.params
+    } catch (err){
+        res.status(500).json({
+            message:err.message
+        })
+    }
+})
+
+router.get("/readAllFrom/:contactName", async (req,res)=>{
+    try{
+        const { contactName }=req.params
+        const findAll=await Message.find({ sender:contactName, receiver:currentUser })
         findAll===0?Error("You do not have any messages from that user"):null
         res.status(200).json(findAll)
     } catch (err){
@@ -19,10 +29,10 @@ router.get("/readAllFrom/:user_id", async (req,res)=>{
     }
 })
 
-router.post("/makePostTo/:user_id", async (req,res)=>{
+router.post("/makePostTo/:contactName", async (req,res)=>{
     try{
-        const { user_id }=req.params
-        const findOne=await User.findOne({user_id})
+        const { contactName }=req.params
+        const findOne=await User.findOne({contactName})
         !findOne?Error("No users by that name exist"):null
         const { body }=req.body
         !body?Error("No Message Content Found"):null
@@ -43,14 +53,14 @@ router.post("/makePostTo/:user_id", async (req,res)=>{
     }
 })
 
-router.put("/updateAt/:_id", async (req,res)=>{
+router.put("/updateAt/:body", async (req,res)=>{
     try{
-        const { _id }=req.params
-        const findOne=await Message.findOne({_id})
+        const { body }=req.params
+        const findOne=await Message.findOne({body})
         !findOne?Error("Message not found"):null
         const newMessage=req.body
         !body?Error("Message content missing"):null
-        const updateOne=await Message.updateOne({_id}, {$set:newMessage})
+        const updateOne=await Message.updateOne({body}, {$set:newMessage})
         res.status(200).json({
             message:"Message Updated"
         })
@@ -61,10 +71,10 @@ router.put("/updateAt/:_id", async (req,res)=>{
     }
 })
 
-router.delete("/deleteAt/:_id", async (req,res)=>{
+router.delete("/deleteAt/:body", async (req,res)=>{
     try{
-        const{_id}=req.params
-        const deleteOne=await Message.findByIdAndDelete({_id})
+        const{body}=req.params
+        const deleteOne=await Message.findByIdAndDelete({body})
         !deleteOne?Error("ID not found"):null
         res.status(200).json({
             message:`Item Deleted`,
