865a8d5cde3105acb9fc4f68559c01a0aadad4b8
Andy <andy.sen@icloud.com>
2023-07-21 10:37:04 -0400

    Routes (#38)
    * routes added to App.js, rename NewM to NewMessage
    
    * added private routes
    
    * fix comment on PrivateRoute

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 
@@ -29,7 +28,7 @@ function App() {
 
       <Route path='/register' element={ <Register /> } />
       <Route path='/login' element={ <Login /> } />
-      // <Route element={ <PrivateRoute /> }>
+      {/* <Route element={ <PrivateRoute /> }> */}
         <Route path='/' element={ <Home /> } />
         <Route path='/invite' element={ <Invite /> } />
         <Route path='/profile' element={ <ReadProfile /> } />
@@ -41,7 +40,7 @@ function App() {
         <Route path='/messaging' element={ <Direct /> } />
         <Route path='/meetbands' element={ <MeetBands /> } />
         <Route path='/news' element={ <News /> } />
-      // </Route>
+      {/* </Route> */}
     </Routes>
   );
 }
