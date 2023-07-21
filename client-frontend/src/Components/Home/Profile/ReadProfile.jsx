824332658afc804231638a10f87997ce5fea6b80
Aaron Blais <abmalachi@gmail.com>
2023-07-21 16:50:12 -0400

    added functions to ReadProfile page

--- client-frontend/src/Components/Home/Profile/ReadProfile.jsx
+++ client-frontend/src/Components/Home/Profile/ReadProfile.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+/* import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
 function ReadProfile() {
+
+  const [ profile, setProfile ] = useState({})
+
+  const sessionToken = localStorage.getItem('token');
+
   const params = useParams();
 
+  const fetchProfile = () => {
+    const url = `http://127.0.0.1:4000/user/${params.user_id}`
+
+    fetch(url, {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application.json",
+        "authorization": sessionToken,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => setProfile(data.foundUser))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchProfile()
+    console.log(" use effect ran")
+  }, [])
+
+  const renderProfile = () => {
+    <h1>{profile.bandName}</h1>
+  }
+
   return (
-    <div>ReadProfile{params.user_id}</div>
+    <div>
+    {params.user_id}
+    {renderProfile()}
+    </div>
   )
 }
 
-export default ReadProfile
+export default ReadProfile */
