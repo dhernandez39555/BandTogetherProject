3182eca0714b4a93983c4772f5b291b564b7fa53
Kyle Pixley <134083496+Kyle-Pixley@users.noreply.github.com>
2023-07-21 10:58:11 -0400

    Kyle mail (#39)
    * bandname
    
    * test
    
    * old structure
    
    * email-route
    
    * typo
    
    * email-sent

--- server-backend/Controllers/nodemailer.js
+++ server-backend/Controllers/nodemailer.js
@@ -4,24 +4,28 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,
     auth: {
-        user: 'BandTogetherBTV@gmail.com',
-        PASS: process.env.PASS,
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
     },
 });
 
 router.post('/send-email', async (req,res) => {
     const { to, text } = req.body
-
+console.log('req',req.body)
     const subject = "Let's Band Together!!"
     try {
+        console.log('cheese')
         await transporter.sendMail({
-            from: 'BandTogetherBTV@gmail.com',
+            from: process.env.EMAIL,
             to,
             subject,
             text,
         });
+        console.log('email sent successfuly')
         res.json({ success: true, message: 'email sent' });
     } catch (err) {
         console.log(err);
@@ -34,5 +38,4 @@ router.get('/test',(req,res) => {
 })
 
 module.exports = router
-
 
