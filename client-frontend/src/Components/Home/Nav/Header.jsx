f61e6416df9b6bf1c8160e566b8c227f9c994891
Andy Sen <andy.sen@icloud.com>
2023-07-23 14:52:37 -0400

    clean up comments in app.jsx and header

--- client-frontend/src/Components/Home/Nav/Header.jsx
+++ client-frontend/src/Components/Home/Nav/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import ProfileIcon from '@mui/icons-material/PersonOutline';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
@@ -8,25 +8,27 @@ function Header() {
     const getUserId = () => {
         const sessionToken = localStorage.getItem('token');
 
-        if (!sessionToken) return;
+        if (!sessionToken) return null;
         try {
-            const decodedToken = jwtDecode(sessionToken)
-            console.log(decodedToken._id)
-            return decodedToken._id 
-        } catch (error) {
-            console.log(`error decoding`,error)
+            const decodedToken = jwtDecode(sessionToken);
+            return decodedToken._id;
+        } catch (err) {
+            console.log(`err decoding`, err);
         }
     }
 
     
 
     return (
+        <>
         <div id="header">
             <div className="nav-wrap">
                 <Link to={`/profile/${getUserId()}`}><ProfileIcon htmlColor='#DB9A35' fontSize='large' /></Link>
                 <Link to="/"><HomeOutlinedIcon htmlColor='#DB9A35' fontSize='large' /></Link>
             </div>
         </div>
+        <Outlet />
+        </>
     )
 }
 
