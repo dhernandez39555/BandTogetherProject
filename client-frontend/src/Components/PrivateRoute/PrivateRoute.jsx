fe0bac815e77540fced035ec83b491cdc35519cc
Andy Sen <andy.sen@icloud.com>
2023-07-25 16:08:03 -0400

    add logo to welcome screen, and fix token undefined issue

--- client-frontend/src/Components/PrivateRoute/PrivateRoute.jsx
+++ client-frontend/src/Components/PrivateRoute/PrivateRoute.jsx
@@ -3,6 +3,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = () => {
     const token = localStorage.getItem("token");
+    console.log(token);
+    if (token === "undefined") localStorage.removeItem("token");
     //TODO add token expired logic
     return  !token ? <Navigate to="/welcome" /> : <Outlet />
 }
