749bdea35f4c6af63efd498f4712dee5b6c00cab
AaronBlais12 <134322726+AaronBlais12@users.noreply.github.com>
2023-07-25 16:04:25 -0400

    Read profile page updates (#66)
    * commented in private routes
    
    * updates
    
    * initial commit
    
    * added icons that link to social media pages
    
    * can embed spotify tracks
    
    * added edit profile navigation
    
    * added cover photo functionality
    
    * adjusted with of iframes
    
    * adjusted iframe width to 50%

--- server-backend/App.js
+++ server-backend/App.js
@@ -20,8 +20,6 @@ const nodemailerController=require("./Controllers/nodemailer")
 const sessionValidation = require("./Middlewares/session")
 
 app.use(cors())
-/* app.use(express.json())
-app.use(express.urlencoded({ extended:true })) */
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
