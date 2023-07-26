7f51f361c639fa31d4d6c18906814704703f8ce5
Andy <andy.sen@icloud.com>
2023-07-26 16:22:40 -0400

    meetbands styled, list bands in 50 miles radius, fixed time in direct messages (#81)

--- client-frontend/src/Components/Home/MeetBands/MeetBands.jsx
+++ client-frontend/src/Components/Home/MeetBands/MeetBands.jsx
@@ -7,68 +7,64 @@ function MeetBands() {
     const [ latitude, setLatitude ] = useState(null)
     const [ longitude, setLongitude ] = useState(null)
 
-
     useEffect(() => {
         const getUsers = async () => {
-        const options = {
-            method: 'GET',
-            headers: new Headers({
-            'Content-Type': 'application/json',
-            authorization: localStorage.getItem('token'),
-            }),
-        };
-    
-        const res = await fetch('http://127.0.0.1:4000/user/all', options);
-        const data = await res.json();
-    
-          // Check if geolocation is available in the browser
-        if ('geolocation' in navigator) {
-            // Get the user's current position
-            navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude: currentLatitude, longitude: currentLongitude } = position.coords;
-                setLatitude(currentLatitude);
-                setLongitude(currentLongitude);
-    
-                const usersWithDistance = data.foundUsers.map((user) => {
-                if (user.latitude && user.longitude) {
-                    const distance = calculateDistance(
-                    currentLatitude,
-                    currentLongitude,
-                    user.latitude,
-                    user.longitude
-                );
-                    return { ...user, distance };
-                }
-                return user;
-            });
-    
-                const sortedUsers = usersWithDistance.sort((a, b) => {
-                if (a.distance === undefined && b.distance === undefined) {
-                    return 0;
-                } else if (a.distance === undefined) {
-                    return 1;
-                } else if (b.distance === undefined) {
-                    return -1;
-                }
-                return a.distance - b.distance;
+            const options = {
+                method: 'GET',
+                headers: new Headers({
+                'Content-Type': 'application/json',
+                authorization: localStorage.getItem('token'),
+                }),
+            };
+        
+            const res = await fetch('http://127.0.0.1:4000/user/all', options);
+            const data = await res.json();
+        
+            // Check if geolocation is available in the browser
+            if ('geolocation' in navigator) {
+                // Get the user's current position
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const { latitude: currentLatitude, longitude: currentLongitude } = position.coords;
+                    setLatitude(currentLatitude);
+                    setLongitude(currentLongitude);
+        
+                    const usersWithDistance = data.foundUsers.map((user) => {
+                        if (user.latitude && user.longitude) {
+                            const distance = calculateDistance(
+                                currentLatitude,
+                                currentLongitude,
+                                user.latitude,
+                                user.longitude
+                            );
+                            return { ...user, distance };
+                        }
+                        return user;
+                    });
+            
+                    const sortedUsers = usersWithDistance.sort((a, b) => {
+                        if (a.distance === undefined && b.distance === undefined) {
+                            return 0;
+                        } else if (a.distance === undefined) {
+                            return 1;
+                        } else if (b.distance === undefined) {
+                            return -1;
+                        }
+                        return a.distance - b.distance;
+                    })
+                    .slice(0, 4)
+                    .filter(user => user.distance < 50);
+        
+                    setUsers(sortedUsers);
+                },
+                (error) => {
+                    console.error('Error getting user location:', error);
                 });
-    
-                setUsers(sortedUsers);
-            },
-            (error) => {
-                console.error('Error getting user location:', error);
+            } else {
+                console.error('Geolocation is not available in this browser.');
             }
-            );
-        } else {
-            console.error('Geolocation is not available in this browser.');
-        }
         };
-    
         getUsers();
     }, []);
-
-
 
     const calculateDistance = (lat1, lon1, lat2, lon2) => {
         const earthRadius = 6371; //kilometers
@@ -91,8 +87,6 @@ function MeetBands() {
         const milesInKm = 0.621371;
         return km * milesInKm;
     };
-    
-
 
     return (
         <div id="meet-page">
@@ -102,19 +96,27 @@ function MeetBands() {
                 ? <h1>Loading</h1>
                 : users.map((user, i) => {
                     return (
-                    <div key={i} className="preview-profile">
-                        <h1>{user.bandName}</h1>
-                        <p className="genre">{user.genre}</p>
-                        <p>{user.bandName}</p>
-                        <p>{user.bio}</p>
-                        {user.distance !== undefined ? ( 
-                            <p>Distance: {convertKmToMiles(user.distance).toFixed(2)} miles</p>
-                        ) :
-                            <p>Distance: N/A</p>
-                        }
-                        <div id="meet-btns">
-                            <Link to={`/profile/${user._id}`}><span>Profile</span></Link>
-                            <Link to={`/messaging/${user._id}`}><span>Message</span></Link>
+                    <div key={i} className="meet-preview-profile">
+                        <div className="meet-cover">
+                            { user.coverPhoto
+                                ? <img src={user.coverPhoto} alt="cover-photo" />
+                                : <h1>{user.bandName}</h1>}
+                        </div>
+                        <div className="meet-bottom">
+                            <div className="meet-img-container">
+                                <img src={ user.profilePicture || "/blank.png" } alt="cover-photo" />
+                            </div>
+                            <div className="meet-header">
+                                <p className="meet-band-name">{user.bandName}</p>
+                                <p className="meet-genre">{user.genre}</p>
+                            </div>
+                            <p className="meet-bio">{user.bio}</p>
+                            <p>{user.location}</p>
+                            <p>{user.distance !== undefined ? `${convertKmToMiles(user.distance).toFixed(2)} miles away` : "Distance: N/A"}</p>
+                            <div id="meet-btns">
+                                <Link to={`/profile/${user._id}`}><span>Profile</span></Link>
+                                <Link to={`/messaging/${user._id}`}><span>Message</span></Link>
+                            </div>
                         </div>
                     </div>   
                     )
