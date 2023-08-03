0f84aa64d95f7358af1c4fc5211a7cb1ee1522c4
KylePixley <kyle_pixley@hotmail.com>
2023-08-03 09:58:32 -0400

    email fix

--- client-frontend/src/Components/Home/Invite.jsx
+++ client-frontend/src/Components/Home/Invite.jsx
@@ -24,6 +24,7 @@ function Invite() {
             method: 'POST',
             headers: {
                 'Content-Type':'application/json',
+                'authorization': localStorage.getItem('token')
             },
             body: JSON.stringify(requestBody),
         })
