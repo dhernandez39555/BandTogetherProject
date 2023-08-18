b57217831d7989558301917a8af516f7dc1f1dc3
Dante Hernandez <hernandezdante2001@gmail.com>
2023-08-18 13:17:19 -0400

    added README for .env

--- server-backend/Utilities/s3.js
+++ server-backend/Utilities/s3.js
@@ -1,8 +1,8 @@
 const aws = require("aws-sdk");
 const crypto = require("crypto");
 
-const region="us-east-1";
-const bucketName="aws-bt-upload-dh";
+const region=process.env.REGION;
+const bucketName=process.env.NAME;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
@@ -12,7 +12,6 @@ const s3 = new aws.S3({
     AWS_SECRET_ACCESS_KEY,
     signatureVersion: "v4"
 });
-console.log(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 const generateUrl = async () => {
     const rawBytes = await crypto.randomBytes(16);
     const imgName = rawBytes.toString("hex");
